f075c57b16ab7a39603e381210633753aa91a5d1
Gaurav Joshi <gauravjoshi1292@gmail.com>
2015-09-14 01:50:03 +0530

    Using selenium to perform dynamic scroll to get clean-sheet stats

--- helpers.py
+++ helpers.py
@@ -1,24 +1,36 @@
 __author__ = 'gj'
 
+import time
 import urllib2
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from urls import *
 
 
 def get_soup_from_url(url):
+    """
+    Given a url returns a soup of page source
+    :param url: web url
+    :return:
+    """
     response = urllib2.urlopen(url)
     html = response.read()
     soup = BeautifulSoup(html, "html.parser")
     return soup
 
 
-def get_table_data():
+def get_league_table_data():
+    """
+    Returns a dictionary containing league table data for all the teams
+    :rtype: dict
+    """
     table_data = {}
     team_data = {}
     team_name = ""
     soup = get_soup_from_url(LEAGUE_TABLE_URL)
     tds = soup.findAll("td")
-    keys = ["played", "won", "drawn", "lost", "goals_for", "goals_against", "goal_diff", "points"]
+    keys = ["played", "won", "drawn", "lost", "goals_for", "goals_against", "goal_diff",
+            "points"]
 
     i = 0
     flag = False
@@ -45,6 +57,10 @@ def get_table_data():
 
 
 def get_player_list():
+    """
+    Returns dicts of players having team, price and points info
+    :rtype: dict, dict, dict, dict
+    """
     goalkeepers = {}
     defenders = {}
     midfielders = {}
@@ -61,13 +77,13 @@ def get_player_list():
         j = 0
         for td in tds:
             if j == 0:
-                player_name = td.text.encode('ascii', 'ignore')
+                player_name = td.text.encode("ascii", "ignore")
             elif j == 1:
-                player_data["team"] = td.text.encode('ascii', 'ignore')
+                player_data["team"] = td.text.encode("ascii", "ignore")
             elif j == 2:
                 player_data["points"] = int(td.text)
             elif j == 3:
-                player_data["price"] = td.text.encode('ascii', 'ignore')
+                player_data["price"] = td.text.encode("ascii", "ignore")
             elif j > 3:
                 if i < 2:
                     goalkeepers[player_name] = player_data
@@ -79,7 +95,7 @@ def get_player_list():
                     forwards[player_name] = player_data
                 player_data = {}
                 j = 1
-                player_name = td.text.encode('utf-8')
+                player_name = td.text.encode("utf-8")
                 continue
 
             j += 1
@@ -89,27 +105,78 @@ def get_player_list():
     return goalkeepers, defenders, midfielders, forwards
 
 
-def get_clean_sheets():
-    from selenium import webdriver
+def get_chrome_driver(url):
+    """
+    Returns a selenium chrome driver
+    :param url:
+    :type url: str
+    :return:
+    """
     driver = webdriver.Chrome()
-    driver.get(GOALKEEPER_CLEAN_SHEETS)
+    driver.get(url)
+    return driver
 
-    e1 = driver.find_element_by_xpath("//option[@value='Goalkeeper Stats']")
-    e2 = driver.find_element_by_xpath("//option[@value='18'][@class='gkeeper']")
 
-    print e1.text
-    print e2.text
+def scroll_to_element(driver, tag_name):
+    """
+    Scroll to elements location in driver's web page
+    :param driver:
+    :type tag_name: str
+    """
+    table_elem = driver.find_element_by_tag_name(tag_name)
+    webdriver.ActionChains(driver).move_to_element(table_elem).perform()
 
-    # e1.click()
-    # e2.click()
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+def get_soup_from_driver(driver):
+    """
+    Given a selenium web driver return the soup
+    :param driver:
+    :return:
+    """
     source = driver.page_source
-    print source
+    soup = BeautifulSoup(source, "html.parser")
+    return soup
 
-    soup = BeautifulSoup(source)
-    print soup
 
-# print get_table_data()
+def get_clean_sheets():
+    """
+    Returns clean sheet data for all the keepers
+    :return:
+    """
+    clean_sheet_data = {}
+    driver = get_chrome_driver(GOALKEEPER_CLEAN_SHEETS)
+    scroll_to_element(driver, "footer")
+    time.sleep(1)
+    soup = get_soup_from_driver(driver)
+    table = soup.find("table", id="ranking-stats-table")
+
+    tds = table.findAll("td")
+    i = 0
+    name, team, matches_played, minutes_played, clean_sheets = "", "", 0, 0, 0
+    for td in tds:
+        try:
+            if td["class"][0] == "table-playerteam-field":
+                name = td.find_next("div",
+                                    {"class": "stats-player-name"}).text.encode("ascii", "ignore")
+                team = td.find_next("div", {"class": "stats-player-team"}).text.encode("ascii", "ignore").split(" - ")[1]
+                continue
+        except KeyError:
+            if i == 0:
+                matches_played = int(td.text)
+                i += 1
+            elif i == 1:
+                minutes_played = int(td.text)
+                i += 1
+            elif i == 2:
+                i = 0
+                clean_sheets = int(td.text)
+                clean_sheet_data[name] = {"team": team,
+                                          "matches_played": matches_played,
+                                          "minutes_played": minutes_played,
+                                          "clean_sheets": clean_sheets}
+    driver.quit()
+    return clean_sheet_data
+
+# print get_league_table_data()
 # print get_player_list()
-get_clean_sheets()
+print get_clean_sheets()
--- urls.py
+++ urls.py
@@ -1,13 +1,11 @@
 __author__ = 'gj'
 
-LEAGUE_TABLE_URL = ("http://www.premierleague.com/en-gb/matchday/league-table.html?season=2014-2015&month=MAY&timeline"
+LEAGUE_TABLE_URL = ("http://www.premierleague.com/en-gb/matchday/league-table.html?season=2015-2016&month=MAY&timeline"
                     "View=date&toDate=1432422000000&tableView=CURRENT_STANDINGS")
 
 PLAYER_LIST_URL = "http://fantasy.premierleague.com/player-list/"
 
 GOALKEEPER_CLEAN_SHEETS = ("http://www.squawka.com/football-player-rankings#clean-sheets#goalkeeper-stats#"
-                           "english-barclays-premier-league|season-2014/2015#all-teams#goalkeeper#16#45#0#0#90#16/08/"
-                           "2014#24/05/2015#season#1#all-matches#total#desc#total")
+                           "english-barclays-premier-league|season-2015/2016#all-teams#goalkeeper#"
+                           "16#43#0#0#90#08/08/2015#13/09/2015#season#1#all-matches#total#desc#total")
 
-("http://www.squawka.com/football-player-rankings#clean-sheets#goalkeeper-stats#english-barclays-premier-league"
- "|season-2014/2015#all-teams#goalkeeper#16#45#0#0#90#16/08/2014#24/05/2015#season#1#all-matches#total#desc#total")
