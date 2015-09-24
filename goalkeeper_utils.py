699b6e50ed193e52d308053750abf1a5d6278b1d
Gaurav Joshi <gauravjoshi1292@gmail.com>
2015-09-25 01:54:53 +0530

    Collect more goalkeeper stats

--- goalkeeper_utils.py
+++ goalkeeper_utils.py
@@ -12,6 +12,15 @@ def get_table_from_url(url, table_class):
     return tds
 
 
+def get_stat_from_text(text, stats_type):
+    if stats_type == 'now_cost':
+        return float(text)
+    elif stats_type == 'selected_by_percent':
+        return float(text.strip('%'))
+
+    return int(text)
+
+
 def scrape_stats_from_table(url_template, stats_type, table_class, column):
     data = {}
 
@@ -23,24 +32,27 @@ def scrape_stats_from_table(url_template, stats_type, table_class, column):
         if not table:
             break
 
-        name, team, value, i = '', '', 0, 0
+        name, team, value, i, check = '', '', 0, 0, False
         for td in table:
-            try:
-                td['class']
-            except KeyError:
-                if i == 2:
+            if len(td.findChildren()) == 1:
+                continue
+
+            if len(td.findChildren()) == 2:
+                i = 0
+                check = True
+                continue
+
+            if check:
+                if i == 0:
                     name = normalize(td.text)
-                elif i == 3:
+                elif i == 1:
                     team = normalize(td.text)
                 elif i == column:
-                    value = float(normalize(td.text))
-
-                if i == 8:
+                    value = get_stat_from_text(normalize(td.text), stats_type)
                     data[(name, team)] = value
-                    i = 0
                     name = ''
                     team = ''
-                    value = 0
+                    check = False
                     continue
 
                 i += 1
@@ -53,23 +65,63 @@ def get_goalkeeper_scores():
     return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
                                    stats_type='total_points',
                                    table_class='ismTable',
-                                   column=8)
+                                   column=6)
 
 
 def get_goalkeeper_prices():
     return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
                                    stats_type='now_cost',
                                    table_class='ismTable',
-                                   column=6)
+                                   column=4)
 
 
 def get_goalkeeper_round_scores():
     return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
                                    stats_type='total_points',
                                    table_class='ismTable',
+                                   column=5)
+
+
+def get_goalkeeper_minutes_played():
+    return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
+                                   stats_type='minutes',
+                                   table_class='ismTable',
                                    column=7)
 
 
-print get_goalkeeper_scores()
-print get_goalkeeper_prices()
-print get_goalkeeper_round_scores()
+def get_goalkeeper_selected_by_percent():
+    return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
+                                   stats_type='selected_by_percent',
+                                   table_class='ismTable',
+                                   column=3)
+
+
+def get_goalkeeper_goals_scored():
+    return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
+                                   stats_type='goals_scored',
+                                   table_class='ismTable',
+                                   column=7)
+
+
+def get_goalkeeper_assists():
+    return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
+                                   stats_type='assists',
+                                   table_class='ismTable',
+                                   column=7)
+
+
+def get_goalkeeper_clean_sheets():
+    return scrape_stats_from_table(url_template=GOALKEEPER_STATS_URL,
+                                   stats_type='clean_sheets',
+                                   table_class='ismTable',
+                                   column=7)
+
+
+# print get_goalkeeper_scores()
+# print get_goalkeeper_prices()
+# print get_goalkeeper_round_scores()
+# print get_goalkeeper_minutes_played()
+# print get_goalkeeper_selected_by_percent()
+# print get_goalkeeper_goals_scored()
+# print get_goalkeeper_assists()
+print get_goalkeeper_clean_sheets()
